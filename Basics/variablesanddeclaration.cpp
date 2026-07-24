870362b05b3efb26763e5179c2a17c45162a5739
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-24 23:28:53 +0530

    different variable types

---
+++ Basics/variablesanddeclaration.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int data type
+    int x=5;
+    cout<<x<<endl;
+    x=8;
+    cout<<x<<endl;
+    x=x-4;
+    cout<<x<<endl;
+    x=x*10;
+    cout<<x<<endl;
+    x=x/3;
+    cout<<x<<endl;
+    // float data type
+    float x = 3.1;
+    cout<<x<<endl;
+    // char data type
+    char ch = 'A';
+   cout<<ch<<endl;
+ }
