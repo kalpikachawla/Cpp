8dee7505ad874cbaac16547fefc47d7b9a53952e
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-25 13:07:34 +0530

    if-else condition based(even or odd)

---
+++ Basics/conditionals/evenorodd.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter x :";
+    cin>>x;
+    if(x%2==0){
+        cout<<"The given number is even.";
+    }
+    else{
+        cout<<"The given number is odd.";
+    }
+}
