69990dca89f689e27c0c1ff66d73fa29e75cf0d5
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-25 09:51:51 +0530

    calculated product of three numbers(Homework)

---
+++ Basics/product.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y,z;
+    cout<<"Enter x :";
+    cin>>x;
+    cout<<"Enter y :";
+    cin>>y;
+    cout<<"Enter z :";
+    cin>>z;
+    cout<<"The product of numbers is :";
+    cout<<x*y*z;
+}
