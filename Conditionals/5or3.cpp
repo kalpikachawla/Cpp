8f5f005ad9cdd46cf1e943757dc2c08ab6ad08ab
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-25 14:27:24 +0530

    checked whether a number is divisible by 5 or 3

---
+++ Conditionals/5or3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter x :";
+    cin>>x;
+    if(x%5==0 || x%3==0){
+        cout<<"Divisible by 5 or 3.";
+    }
+    else{
+        cout<<"Not divisible by 5 or 3.";
+    }
+}
