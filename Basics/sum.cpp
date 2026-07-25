ea6c950d401950f49ef7a31027043165f0273d22
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-25 09:47:27 +0530

    calculated sum of two numbers by taking input

---
+++ Basics/sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter x :";
+    cin>>x;
+    cout<<"Enter y :";
+    cin>>y;
+    cout<<"The sum of x and y is :";
+    cout<<x+y;
+    
+}
