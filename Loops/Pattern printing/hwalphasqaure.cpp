2352c28e565e20f9bf02e0907b0baa98016aad44
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-26 10:39:20 +0530

    printed alphabet sqaure (homework)

---
+++ Loops/Pattern printing/hwalphasqaure.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"Enter n :";
+    cin>>n;
+    cout<<"Enter m :";
+    cin>>m;
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
+            cout<<(char)(i+64);
+        }
+        cout<<endl;
+    }
+}
