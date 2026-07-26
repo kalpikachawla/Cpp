6c19b9b56ca342194c24be163a7885c1fb54fdab
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-26 14:41:07 +0530

    printed star pyramid

---
+++ Loops/Pattern printing/starpyramid.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+    int nsp=n-1; 
+    int nst=1;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=nsp; j++){
+            cout<<" ";
+        }
+        for(int k=1; k<=nst; k++){
+            cout<<"*";
+        }
+        nsp--;
+        nst+=2;
+       cout<<endl;
+    }
+}
