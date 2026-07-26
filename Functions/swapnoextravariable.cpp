8b226eae47f956367549a554342176cc4da8c04b
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-26 18:56:44 +0530

    swapped numbers using same variables

---
+++ Functions/swapnoextravariable.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=90;
+    int b=80;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<"The value of a is :"<<a<<endl;
+    cout<<"The value of b is :"<<b;
+}
