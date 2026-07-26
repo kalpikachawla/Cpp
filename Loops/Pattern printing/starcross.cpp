06da430ec2fccc9e1cff904cf01b9347313d331f
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-26 14:10:30 +0530

    printed star cross

---
+++ Loops/Pattern printing/starcross.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m; 
+    cout<<"Enter n :";
+    cin>>n;
+    cout<<"Enter m :";
+    cin>>m;
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
+            if(i==j || i+j==n+1){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    
+}
