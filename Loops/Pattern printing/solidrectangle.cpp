f7ca056a442ce88be219fad0a3968d3058bc3ab4
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-26 10:17:08 +0530

    printed solid rectangle

---
+++ Loops/Pattern printing/solidrectangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"Enter columns :";
+    cin>>n;
+    cout<<"Enter rows :";
+    cin>>m;
+    for(int i=1; i<=m; i++){
+        cout<<"*****"<<endl;
+    }
+
+}
