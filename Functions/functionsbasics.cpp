d34b4a0fae77f32502efa28d7fe067c0da17f36c
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-26 17:26:11 +0530

    basics of functions

---
+++ Functions/functionsbasics.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void greet(){
+    cout<<"Good Morning"<<endl;
+    cout<<"How are you?"<<endl;
+}
+void India(){
+    cout<<"Namaste"<<endl;
+    cout<<"Aap kaise hai?";
+}
+int main(){
+    cout<<"You are in main Function"<<endl;
+    greet();
+    India();
+}
