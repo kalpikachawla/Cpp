548d1f657ba142158167659241afc9e45df07929
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-26 10:37:27 +0530

    printed alphabet sqaure

---
+++ Loops/Pattern printing/alphabetsuare.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"Enter n :";
+    cin>>n;
+    cout<<"Enter m :";
+    cin>>m;
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
+            cout<<(char)(j+64)<<" ";
+        
+        }
+        cout<<endl;
+    }
+}
