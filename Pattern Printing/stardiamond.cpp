05d2518e2ab9657130ccb9b872a4c03a6dcadef9
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-27 10:49:19 +0530

    printed star diamond

---
+++ Pattern Printing/stardiamond.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+     int n;
+    printf("Enter the number of lines :");
+    scanf("%d",&n);
+    int nsp= n/2;
+    int nst=1;
+    int ml=n/2+1;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=nsp; j++)
+        printf(" ");
+         for(int k=1; k<=nst; k++)
+        printf("*");
+        if(i<ml){
+            nsp--;
+            nst+=2;
+        }
+        else{
+            nsp++;
+            nst-=2;
+}
+cout<<endl;
+    }
+}
