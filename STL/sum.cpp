6984c461fbc6fec16f5e4550dc0c7b526c6851f1
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-07-31 14:02:54 +0530

    calculated sum of elements in a vector

---
+++ STL/sum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>v;
+    int n=5;
+    cout<<"Enter all the elements :\n";
+    for(int i=0; i<n; i++){
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+    int sum = 0;
+    for(int i=0; i<v.size(); i++){
+        sum+=v[i];
+    }
+    cout<<"Sum of elements is : "<<sum;
+}
+    
