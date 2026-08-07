a7d511f417f5cffd59061a952eeeac7fddfa3b49
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-08-07 10:20:33 +0530

    prime or not

---
+++ Practice/prime.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+    int count = 0;
+    for(int i=1; i<=n; i++){
+        if(n%i==0) count++;
+    }
+    if(count==2){
+        cout<<"Prime number";
+    }
+    else{
+        cout<<"Not a prime number";
+    }
+}
