7bed7c341f661c04346d912fa228472b26e35a35
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-08-07 13:51:31 +0530

    printed fibonacci terms

---
+++ Practice/fibonaci.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+    int first=0, second = 1, next;
+    for(int i=1; i<=n; i++){
+        cout<<first<<" ";
+        next = first + second;
+        first = second;
+        second = next;
+    }
+   
+}
