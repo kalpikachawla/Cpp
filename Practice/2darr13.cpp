54d4d835f0f6cb93d0598ca2d1215cddb25abb98
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-08-10 12:23:09 +0530

    printed difference of sum of main diagonal and secondary diagonal

---
+++ Practice/2darr13.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r,c;
+    cout<<"Enter r :";
+    cin>>r;
+    cout<<"Enter c :";
+    cin>>c;
+    int arr[r][c];
+    cout<<"Enter all the elements :\n";
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl;
+    cout<<"Matrix :\n";
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    int sum = 0;
+    for(int i=0; i<r; i++){
+       sum+=arr[i][i];
+        
+    }
+    int sums=0;
+    cout<<"Sum of main diagonal : "<<sum<<endl;
+    for(int i=0; i<r; i++){
+        sums+=arr[i][c-i-1];
+    }
+    cout<<"Sum of secondary diagonal : "<<sums<<endl;
+    int difference = sum-sums;
+    cout<<"Difference :"<<difference<<endl;
+}
