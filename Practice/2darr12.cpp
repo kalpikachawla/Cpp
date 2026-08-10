87753766eba542c59a462e5f289567f44656635b
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-08-10 12:17:47 +0530

    printed sum of secondary diagonal

---
+++ Practice/2darr12.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r,c;
+    cout<<"Enter r :";
+    cin>>r;
+    cout<<"Enter c :";
+    cin>>c;
+    int arr[r][c];
+    cout<<"Enter all the elements :\n";
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl;
+    cout<<"Matrix :\n";
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    int sum = 0;
+    for(int i=0; i<r; i++){
+       sum+=arr[i][c-i-1];
+        
+    }
+    cout<<"Sum of secondary diagonal : "<<sum<<endl;
+}
