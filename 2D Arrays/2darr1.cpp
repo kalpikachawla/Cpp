7b6e1a70156978ed7e201d93dc3c336e9e7336da
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-08-13 12:48:23 +0530

    printed transposed matrix

---
+++ 2D Arrays/2darr1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r,c;
+    cout<<"Enter r :";
+    cin>>r;
+    cout<<"Enter c :";
+    cin>>c;
+    int arr[r][c];
+    cout<<"Enter all the elements :\n";
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl<<"Actual matrix :\n";
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl<<"Transposed matrix :\n";
+    for(int i=0; i<c; i++){
+        for(int j=0; j<r; j++){
+            cout<<arr[j][i]<<" ";
+        }
+         cout<<endl;
+    }
+}
