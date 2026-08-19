2ef13959af0338d4e071a039291c752b011e4c22
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-08-19 10:51:58 +0530

    Reversed array

---
+++ Arrays/arr7.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter size :";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter all the elements :\n";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    cout<<"Reversed array :\n";
+    for(int i=n-1;i>=0; i--){
+        cout<<arr[i]<<" ";
+    }
+}
