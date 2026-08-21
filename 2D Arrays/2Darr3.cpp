d3a7f5fe2d06b6ae0f8ea8831b5bf3b74c123fc9
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-08-21 13:39:15 +0530

    sum of row

---
+++ 2D Arrays/2Darr3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r,c;
+    cout<<"Enter r :";
+    cin>>r;
+    cout<<"Enter c :";
+    cin>>c;
+    int arr[r][c];
+    cout<<"Enter all the elements :\n";
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<"Matrix :\n";
+    for(int i=0; i<r; i++){
+        for(int j=0; j<c; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i=0; i<r; i++){
+        int sum = 0;
+        for(int j=0; j<c; j++){
+            sum+=arr[i][j];
+        }
+        cout<<"Sum of row "<<i+1<<" : "<<sum<<endl;
+    } 
+}
