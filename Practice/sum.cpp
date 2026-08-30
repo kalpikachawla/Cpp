db92fd075b646e5799f7b2f8d8539feed788eb73
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-08-30 21:31:46 +0530

    sum of 2d matrix

---
+++ Practice/sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int arr[2][3]={1,3,4,6,7,2};
+   int sum = 0;
+   for(int i=0; i<2; i++){
+    for(int j=0; j<3; j++){
+        sum+=arr[i][j];
+    }
+   }
+   cout<<"SUM : "<<sum;
+
+}
